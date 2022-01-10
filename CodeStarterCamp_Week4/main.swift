e9d4f3766b74b1c6c0548aacaf9525d4ceb48241
yoosoony <ystar819@naver.com>
2022-01-10 22:07:45 +0900

    feat : Create the structures and various exercises

--- CodeStarterCamp_Week4/main.swift
+++ CodeStarterCamp_Week4/main.swift
@@ -8,5 +8,34 @@
 
 import Foundation
 
-print("Hello, World!")
+struct BodyCondition {
+    var upperBodyStrength: Int
+    var lowerBodyStrength: Int
+    var muscularEndurance: Int
+    var fatigue: Int
+}
+
+struct Exercise {
+    let name: String
+    let action: () -> Void
+}
+
+let sitUp: Exercise = Exercise(name: "Sit-Up", action: {
+
+})
+
+let squat: Exercise = Exercise(name: "Squat", action: {
+    
+})
+
+let longRun: Exercise = Exercise(name: "Long-Run", action: {
+    
+})
+
+let activeRest: Exercise = Exercise(name: "Active-Rest", action: {
+    
+})
+
+
+
 
